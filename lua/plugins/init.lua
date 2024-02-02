d283a2304326d06357d0d11a7d43b811173c3db5
thebromo <manuel@strenge.ch>
2024-02-02 21:49:35 +0000

    fixed spelling

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,4 @@
-ocal lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   -- clone lazy if not already exists
   vim.fn.system({
