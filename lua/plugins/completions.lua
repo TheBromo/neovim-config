02f4be0059e6bb789f814c2503c8de0d8c41738d
Manuel Strenge <manuel@strenge.ch>
2024-02-06 22:46:15 +0100

    added autoformat

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,6 +1,6 @@
 return {
   "hrsh7th/cmp-nvim-lsp",
-  'hrsh7th/cmp-path',
+  "hrsh7th/cmp-path",
   {
     "L3MON4D3/LuaSnip",
     dependencies = {
@@ -12,31 +12,53 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
       require("luasnip.loaders.from_vscode").lazy_load()
+      luasnip.config.setup({})
 
       cmp.setup({
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
+        completion = {
+          completeopt = "menu,menuone,noinsert",
         },
         mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete({}),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
-        sources = cmp.config.sources({
+        sources = {
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
-        }, {
-          { name = "buffer" },
-        }),
+          { name = "luasnip" },
+          { name = "path" },
+        },
       })
     end,
   },
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,10 +1,10 @@
 return {
-	"nvimtools/none-ls.nvim",
+--[[	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
+				--				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.erb_lint,
 				null_ls.builtins.diagnostics.eslint_d,
@@ -12,7 +12,5 @@ return {
 				null_ls.builtins.formatting.rubocop,
 			},
 		})
-
---		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+	end,--]]
 }
