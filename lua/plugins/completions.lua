262e8b43fcc773bf5b635679df443c4086b8a024
thebromo <manuel@strenge.ch>
2024-02-07 16:49:41 +0000

    changed some stuff cause of nix flake behaviour

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,65 +1,122 @@
 return {
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-path",
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+
+    {
+      'hrsh7th/cmp-vsnip',
+      dependencies = { 'hrsh7th/vim-vsnip' },
     },
   },
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      require("luasnip.loaders.from_vscode").lazy_load()
-      luasnip.config.setup({})
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete({}),
-          ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
+  event = 'InsertEnter',
+
+  opts = function()
+    local cmp = require('cmp')
+    local utils = require('utils')
+    local devicons = require('nvim-web-devicons')
+
+    local icons = {
+      Text           =  '󰦨',
+      Method         =  '',
+      Function       =  '󰊕',
+      Constructor    =  '',
+      Field          =  '',
+      Variable       =  '󰫧',
+      Class          =  '',
+      Interface      =  '',
+      Module         =  '',
+      Property       =  '',
+      Unit           =  '󰑭',
+      Value          =  '',
+      Enum           =  '',
+      Keyword        =  '',
+      Snippet        =  '',
+      Color          =  '',
+      File           =  '',
+      Reference      =  '',
+      Folder         =  '',
+      EnumMember     =  '',
+      Constant       =  '',
+      Struct         =  '',
+      Event          =  '',
+      Operator       =  '',
+      TypeParameter  =  '',
+    };
+
+    return {
+      preselect = 'none',
+
+      snippet = {
+        expand = function(args)
+          vim.fn["vsnip#anonymous"](args.body)
+        end,
+      },
+
+      mapping = {
+        -- close/abort
+        ['<C-e>'] = { i = cmp.mapping.close() },
+        ['<C-c>'] = { i = cmp.mapping.abort() },
+
+        -- accept
+        ['<C-y>'] = { i = cmp.mapping.confirm({ select = false }) },
+
+        -- item selection
+        ['<C-p>'] = {
+          i = function()
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
             else
-              fallback()
+              cmp.complete()
             end
-          end, { "i", "s" }),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
+          end,
         },
-      })
-    end,
-  },
+        ['<C-n>'] = {
+          i = function()
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              cmp.complete()
+            end
+          end,
+        },
+      },
+
+      formatting = {
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(_, vim_item)
+          vim_item.kind = string.format('%s', icons[vim_item.kind])
+          return vim_item
+        end,
+      },
+
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+        {
+          name = 'buffer',
+          option = {
+            get_bufnrs = function()
+              local all_buffers = vim.api.nvim_list_bufs()
+              local selected_buffers = {}
+
+              for _, buf in pairs(all_buffers) do
+                -- prevent trying to complete stuff from huge files
+                if not utils.is_large_buffer(buf) then
+                  table.insert(selected_buffers, buf)
+                end
+              end
+
+              return selected_buffers
+            end
+          },
+        },
+        { name = 'path' },
+      }),
+    }
+  end,
 }
