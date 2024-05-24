189b87d9b13d3072efc8412114703731fa779457
thebromo <manuel@strenge.ch>
2024-05-24 13:37:16 +0000

    removed nf dependence

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,13 +1,27 @@
 return {
-	{ -- Collection of various small independent plugins/modules
-		"echasnovski/mini.nvim",
-		config = function()
-			local statusline = require("mini.statusline")
-			statusline.setup()
-
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
-		end,
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				theme = "moonfly",
+				section_separators = "",
+				component_separators = "",
+				icons_enabled = false,
+			},
+		},
 	},
+	-- { -- Collection of various small independent plugins/modules
+	-- 	"echasnovski/mini.nvim",
+	-- 	config = function()
+	-- 		local statusline = require("mini.statusline")
+	-- 		statusline.setup({
+	-- 			use_icons = false,
+	-- 		})
+	--
+	-- 		statusline.section_location = function()
+	-- 			return "%2l:%-2v"
+	-- 		end
+	-- 	end,
+	-- },
 }
