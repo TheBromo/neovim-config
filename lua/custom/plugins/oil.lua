9ee367fe96c5ff62d00cff4f4aab6d80074e52ff
thebromo <manuel@strenge.ch>
2024-07-09 10:27:11 +0000

    trying to fix oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,16 +1,19 @@
 return {
-	"stevearc/oil.nvim",
+    "stevearc/oil.nvim",
 
-	event = "VeryLazy",
+    event = "VeryLazy",
 
-	config = function()
-		require("oil").setup()
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	end,
+    config = function()
+        require("oil").setup()
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
 
-	opts = {
-		view_options = {
-			show_hidden = true,
-		},
-	},
+    opts = {
+        default_file_explorer = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+            show_hidden = true,
+        },
+
+    },
 }
