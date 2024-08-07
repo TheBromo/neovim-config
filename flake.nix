108d5d8f5c727e82c121fa2c651a0c18e3325018
thebromo <manuel@strenge.ch>
2024-08-07 23:18:50 +0200

    added flake

---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Import nixpkgs with specific system
+        pkgs = import nixpkgs {
+          inherit system; # Change this according to your system architecture
+        };
+
+        # The list of dependencies
+        runtimeDeps = [
+          pkgs.stdenv.cc
+          pkgs.cargo
+          pkgs.curl
+          pkgs.fd
+          pkgs.fzf
+          pkgs.git
+          pkgs.gnumake
+          pkgs.gnused
+          pkgs.gnutar
+          pkgs.gzip
+          pkgs.lua-language-server
+          pkgs.neovim
+          pkgs.nodejs
+          pkgs.nodePackages.neovim
+          pkgs.ripgrep
+          pkgs.tree-sitter
+          pkgs.unzip
+        ];
+      in
+      {
+        # Define the default package
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "nvim";
+            version = "1.0.0";
+            src = ./.;
+
+            nativeBuildInputs = [
+              pkgs.gnused
+              pkgs.makeWrapper
+            ];
+
+            buildInputs = runtimeDeps;
+
+            buildPhase = ''
+              runHook preBuild
+
+              mkdir -p share/nvim
+              cp init.lua  share/nvim
+              cp -r lua share/nvim
+
+              mkdir bin
+              cp utils/bin/nvim.template bin/nvim
+              chmod +x bin/nvim
+
+              substituteInPlace bin/nvim \
+                 --replace RUNTIME_DIR_VAR \$HOME/.local/share/nvim \
+                 --replace CONFIG_DIR_VAR \$HOME/.config/nvim \
+                 --replace CACHE_DIR_VAR \$HOME/.cache/nvim \
+                 --replace BASE_DIR_VAR $out/share/nvim \
+                 --replace neovim-binary ${pkgs.neovim}/bin/nvim
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out
+              cp -r bin share $out
+
+              wrapProgram $out/bin/nvim --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps} \
+                --prefix LD_LIBRARY_PATH : ${pkgs.stdenv.cc.cc.lib} \
+                --prefix CC : ${pkgs.stdenv.cc.targetPrefix}cc
+
+              runHook postInstall
+            '';
+
+            meta = with pkgs.lib; {
+              description = "IDE layer for Neovim";
+              homepage = "https://www.strenge.ch/";
+              platforms = platforms.unix;
+              mainProgram = "nvim";
+            };
+          };
+        };
+      });
+}
