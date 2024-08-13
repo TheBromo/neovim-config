aebf3e30a284592774c57c640d0c4fb13cb86ffb
thebromo <manuel@strenge.ch>
2024-08-13 10:49:22 +0000

    updated flake and rm nil from mason(not cross plat)

--- lua/custom/plugins/mason.lua
+++ lua/custom/plugins/mason.lua
@@ -114,15 +114,6 @@ return {
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
             local servers = {
-                nil_ls = {
-                    settings = {
-                        nixd = {
-                            formatting = {
-                                command = "nixfmt-rfc-style",
-                            },
-                        },
-                    },
-                },
                 yamlls = {
                     settings = {
                         yaml = {
