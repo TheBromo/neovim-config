c46eea74ad7c2f77c2c840e69ad6f4c6b5759d49
thebromo <manuel@strenge.ch>
2024-11-16 17:00:43 +0100

    added typescript

--- lua/custom/plugins/autoformat.lua
+++ lua/custom/plugins/autoformat.lua
@@ -12,6 +12,7 @@ return {
                 rust = { "rustfmt", lsp_format = "fallback" },
                 -- Conform will run the first available formatter
                 javascript = { "prettierd", "prettier", stop_after_first = true },
+                typescript = { "prettierd", "prettier", stop_after_first = true },
 
                 go = { "gofmt" },
             },
