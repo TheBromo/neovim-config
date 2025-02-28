10931db038085587ada9c25086041831195db59b
thebromo <manuel@strenge.ch>
2025-02-28 09:04:09 +0100

    fix oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,20 +4,16 @@ return {
 	event = "VeryLazy",
 
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			default_file_explorer = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+				is_hidden_file = function(name, _)
+					return vim.startswith(name, ".")
+				end,
+			},
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
-
-	opts = {
-		default_file_explorer = true,
-		skip_confirm_for_simple_edits = true,
-		view_options = {
-			-- Show files and directories that start with "."
-			show_hidden = true,
-			-- This function defines what is considered a "hidden" file
-			is_hidden_file = function(name, _)
-				return vim.startswith(name, ".")
-			end,
-		},
-	},
 }
